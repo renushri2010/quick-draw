e8dcab074568138a36150e656ae10772d2dba851
renushri2010 <90689502+renushri2010@users.noreply.github.com>
2022-07-25 20:56:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ drawn_sketch = "";
 answer_holder = "";
 score = 0;
 
-function update()
+function updateCanvas()
 {
     background("white");
     random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
@@ -20,21 +20,53 @@ function update()
     document.getElementById('sketch_name').innerHTML = 'Sketch to be drawn : ' + sketch;
 }
 
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
 function setup()
 {
     canvas = createCanvas(280, 280);
     canvas.center();
-    background("white")
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
 }
 
 function draw()
 {
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed)
+    {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
     check_sketch();
     if(drawn_sketch == sketch)
     {
         answer_holder = "set";
         score++;
         document.getElementById('score').innerHTML = 'Score : ' + score;
+    }
+}
+
+function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        drawn_sketch = results[0].label;
+        document.getElementById('label').innerHTML = 'Your Sketch : ' + drawn_sketch;
+        document.getElementById('confidence').innerHTML = 'Confidence : ' + Math.round(results[0].confidence * 100) + '%';
     }
 }
 
